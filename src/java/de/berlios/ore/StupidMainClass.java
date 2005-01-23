f95a62bb063a19b0f0e931dbcdb04c14f2f28ac5
skandalfo <skandalfo>
2005-01-23 20:19:19 +0000

    - added skeleton and stupid class for netbeans 4.0 development.

---
+++ src/java/de/berlios/ore/StupidMainClass.java
@@ -0,0 +1,19 @@
+/*
+ * StupidMainClass.java
+ *
+ * Created on 23 de enero de 2005, 20:21
+ */
+
+package de.berlios.ore;
+
+/**
+ *
+ * @author skandalfo
+ */
+public class StupidMainClass {
+    
+    public static void main(String [] args) {
+        System.out.println("Oh! What a stupid class I am...");
+    }
+    
+}
