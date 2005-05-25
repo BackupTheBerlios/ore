67eefa23b912cc41b4536e8f9f4fea753783395e
skandalfo <skandalfo>
2005-05-25 19:42:15 +0000

    - Stupid main class being used for some merciless testing...

--- src/java/de/berlios/ore/StupidMainClass.java
+++ src/java/de/berlios/ore/StupidMainClass.java
@@ -6,6 +6,9 @@
 
 package de.berlios.ore;
 
+import org.objectweb.jotm.Jotm;
+import org.objectweb.transaction.jta.TMService;
+
 /**
  *
  * @author skandalfo
@@ -14,6 +17,12 @@ public class StupidMainClass {
     
     public static void main(String [] args) {
         System.out.println("Oh! What a stupid class I am...");
+        try {
+        	TMService jotm = new Jotm(true, true);
+        }
+        catch(Exception e) {
+        	e.printStackTrace();
+        }
     }
     
 }
